6e1acce79336d445741a8ebb2120e292179927f2
Anh <duyanh2005@gmail.com>
2018-09-23 21:24:02 +0700

    - first aws function

---
+++ handler.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports.hello = async (event, context) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  return response;
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.getCurrentDate = async (event, context) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      date: new Date()
+    }),
+  };
+  
+  return response;
+}
