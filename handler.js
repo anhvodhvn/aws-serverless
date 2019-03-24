2866c17492c4bb6b902ff1094f68ff17ed140cf9
devinsg <duyanh2005@gmail.com>
2019-03-24 13:19:27 +0000

    update api get user list

--- handler.js
+++ handler.js
@@ -1,32 +1,33 @@
 'use strict';
 const moment = require('moment');
 const info = require('package.json');
+const userService = require('./services/userService');
 
-module.exports.hello = async (event, context) => {
+module.exports.userlist = async (event, context) => {
+  let users = userService.getUserList();
   let response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      users: users
     }),
   };
 
   return response;
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
 
 module.exports.now = async (event, context) => {
   let response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: moment(new Date()).format('YYYY-MM-DD HH:mm:ss'),
+      message: "Go Serverless v1.0! Your function executed successfully!",
+      date: moment(new Date()).format('YYYY-MM-DD HH:mm:ss'),
       input: event,
     }),
   };
   
   return response;
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
 
 module.exports.status = async (event, context) => {
@@ -35,8 +36,7 @@ module.exports.status = async (event, context) => {
     body: JSON.stringify({
       version: info.version,
       name: info.name,
-      date: moment(new Date()).format('YYYY-MM-DD HH:mm:ss'),
-      input: event,
+      date: moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
     }),
   };
   
